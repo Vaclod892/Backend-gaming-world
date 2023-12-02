20aa716353371be86bd3c8ca3e0e53a793a4645f
Facufaf <fuertesfaf2005@gmail.com>
2023-12-02 20:50:22 -0300

    actualizacion del get

--- server/routes/api/juegos.js
+++ server/routes/api/juegos.js
@@ -4,9 +4,9 @@ var con = require("./conexion");
 
 
 router.post('/', function(req, res, next) {
-    const {nombre, genero_id, desarrolladora_id, plataforma_id}=req.body;
-    const sql="INSERT INTO juegos(nombre, genero_id, desarrolladora_id, plataforma_id)VALUES(?,?,?,?)";
-    con.query(sql, [nombre, genero_id, desarrolladora_id, plataforma_id],function(error, result){
+    const {nombre, genero_id, desarrolladora_id, plataforma_id, precio}=req.body;
+    const sql="INSERT INTO juegos(nombre, genero_id, desarrolladora_id, plataforma_id, precio)VALUES(?,?,?,?,?)";
+    con.query(sql, [nombre, genero_id, desarrolladora_id, plataforma_id, precio],function(error, result){
     
         if (error){
             res.json({
@@ -59,6 +59,26 @@ router.put('/:juegos_id', function(req, res, next) {
         }
     })
 });
+
+ router.get('/:juegos_id', function(req, res, next){
+     const {authorization}= req.headers;
+     console.log(authorization);
+     const sql='SELECT* FROM juegos where juegos_id=' + req.params.juegos_id
+     con.query(sql, function(error, result){
+         
+         if (error){
+             res.json({
+                 status:"error" ,
+                 error
+             })
+         } else {
+             res.json({
+                 status:"ok",
+                 juegos:result
+             });
+         }
+     })
+ });
 
 
 
